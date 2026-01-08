d330dd314bab1ce40c4887c6908e688ea6465927
Fernand084 <edgar.lopezr84@outlook.com>
2026-01-07 21:23:45 -0600

    TcpServer class reflect now the usage of the new HTTP and Router classes to achieve better structure, an explicit HTTP model, extensible code. Also create the foundation to implement POST, filters/middlewares.

--- src/main/java/server/TcpServer.java
+++ src/main/java/server/TcpServer.java
@@ -5,7 +5,11 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.http.HttpRequest;
 import java.time.LocalDateTime;
+
+import server.http.HttpResponse;
+import server.router.Router;
 
 /**
  * TcpServer
@@ -16,83 +20,21 @@ public class TcpServer {
         int port = 8080;
         System.out.println("Starting TCP server on port "+port+"...");
 
+        Router router = new Router();
+
         try (ServerSocket serverSocket = new ServerSocket(port)) {
            // server is listening for connections
             while (true) {
                 Socket clientSocket = serverSocket.accept(); // Blocks thread and awaits for a client to connect
-                System.out.println("Client connected: "+clientSocket.getInetAddress());
 
                 BufferedReader reader = new BufferedReader(
                     new InputStreamReader(clientSocket.getInputStream())
                 );
 
-                OutputStream out = clientSocket.getOutputStream();
+                HttpRequest request = new HttpRequest(reader);
+                HttpResponse response = router.route(request);
 
-                // 1 read request line
-                String requestLine = reader.readLine();
-                if (requestLine == null || requestLine.isEmpty()) {
-                    clientSocket.close();
-                    continue;
-                }
-                System.out.println("Request: "+requestLine);
-
-                // 2 parse request line
-                String[] parts = requestLine.split(" ");
-                String method = parts[0];
-                String path = parts[1];
-
-                // 3 read and discard headers
-                String line;
-                while ((line = reader.readLine()) != null && !line.isEmpty()) {
-                    // ignored headers
-                }
-
-                // 4 routing
-                String responseBody;
-                int statusCode;
-                String statusText;
-
-                if (!method.equals("GET")) {
-                    statusCode = 405;
-                    statusText = "Method not allowed";
-                    responseBody = "Only GET is supported";
-                }else{
-                    switch (path) {
-                        case "/":
-                            statusCode = 200;
-                            statusText = "OK";
-                            responseBody = "Welcome to my Java HTTP Server!";
-                            break;
-                        case "/hello":
-                            statusCode = 200;
-                            statusText = "OK";
-                            responseBody = "Hello from Java!";
-                            break;
-                        case "/time":
-                            statusCode = 200;
-                            statusText = "OK";
-                            responseBody = "Server time: " + LocalDateTime.now();
-                            break;
-                        default:
-                            statusCode = 404;
-                            statusText = "Not Found";
-                            responseBody = "404 - Page NOT found";
-                    }
-                }
-
-
-                // 5 build http response
-                String response =
-                    "HTTP/1.1 " + statusCode + " " + statusText + "\r\n" +
-                    "Content-Type: text/plain\r\n" +
-                    "Content-length: " + responseBody.length() + "\r\n" +
-                    "\r\n" +
-                    responseBody;
-
-                // 6 send response
-                out.write(response.getBytes());
-                out.flush();
-                
+                clientSocket.getOutputStream().write(response.toBytes()); 
                 clientSocket.close();
             }
         } catch (Exception e) {
