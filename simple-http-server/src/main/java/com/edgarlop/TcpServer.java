56e754772297191a2e9b0830efb2428f8a76d5ba
Fernand084 <edgar.lopezr84@outlook.com>
2026-01-11 21:15:18 -0600

    Phase5 - ThreadPool: initially phase5 uses a 1-thread-per-connection to help understand concurrency and the disadvantajes of this approach. Phase5_ThreadPool now implements a fixed thread pool size to handle all incoming requests(Runnable), we now create a new ClientHandler (not a thread) and is sent to the pool, a free thread handles it and then goes back to the pool (thread does not die), this helps to handle resources since thread creation is expensive.

--- simple-http-server/src/main/java/com/edgarlop/TcpServer.java
+++ simple-http-server/src/main/java/com/edgarlop/TcpServer.java
@@ -5,6 +5,8 @@ import com.edgarlop.server.ClientHandler;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * TcpServer
@@ -16,16 +18,14 @@ public class TcpServer {
         System.out.println("HTTP Server running  on port "+port+"...");
 
         Router router = new Router();
+        ExecutorService threadPool = Executors.newFixedThreadPool(10);
 
         try (ServerSocket serverSocket = new ServerSocket(port)) {
            // server is listening for connections
             while (true) {
                 Socket clientSocket = serverSocket.accept(); 
 
-                Thread thread = new Thread(
-                    new ClientHandler(clientSocket, router)
-                );
-                thread.start();
+                threadPool.submit(new ClientHandler(clientSocket, router));
             }
         } catch (Exception e) {
             // TODO: handle exception
